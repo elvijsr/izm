04efe94726ae9aa83a99e1b591e062df6093a6b2
elvijsr <erascevs@gmail.com>
2023-06-03 21:17:12 +0300

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Landing from './pages/Landing.jsx'
-import Layout from './pages/Layout.jsx'
-import ErrorPage from './pages/errorPage.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Landing from "./pages/Landing.jsx";
+import Layout from "./pages/Layout.jsx";
+import ErrorPage from "./pages/errorPage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const theme = createTheme({
   palette: {
@@ -22,32 +21,33 @@ const theme = createTheme({
       main: "#4389A2",
     },
     text: {
-      body: "#FFFFFF"
-    }
+      body: "#FFFFFF",
+    },
   },
   typography: {
-    color: "#000000"
+    color: "#000000",
   },
 });
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <Layout />,
-  errorElement: <ErrorPage />,
-  children: [
-    {
-      path: "",
-      element: <Landing />
-    }
-  ]}
-])
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <Landing />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
