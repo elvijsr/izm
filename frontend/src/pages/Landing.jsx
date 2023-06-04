f6f720b74b978d451a1519a7721e499f7c4b032a
arturs-skutans <arturs.skutans@lokalise.com>
2023-06-05 02:48:03 +0300

    radius slider

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -8,6 +8,7 @@ import {
   ListItemButton,
   ListItemText,
   Link,
+  Slider,
 } from "@mui/material";
 import SchoolListItem from "../components/SchoolListItem";
 import { useEffect, useState } from "react";
@@ -21,11 +22,27 @@ export default function Landing() {
   );
   const [allSchools, setAllSchools] = useState(null);
   const [filteredSchools, setFilteredSchools] = useState(null);
+  const [radiusFilter, setRadiusFilter] = useState(2);
   const fetchSchoolsFromAPI = async () => {
     const response = await fetchSchools();
     console.log(response);
     setAllSchools(response.data);
   };
+
+  const handleRadiusFilter = (event, newValue) => {
+    setRadiusFilter(newValue);
+  };
+
+  const marks = [
+    {
+      value: 1,
+      label: "1 km",
+    },
+    {
+      value: 5,
+      label: "5 km",
+    },
+  ];
 
   useEffect(() => {
     fetchSchoolsFromAPI();
@@ -48,11 +65,29 @@ export default function Landing() {
             schools={allSchools}
             setFilteredSchools={setFilteredSchools}
             origin={address}
-            schoolRadius={2}
+            schoolRadius={radiusFilter}
           />
         )}
       </Box>
-      {filteredSchools && filteredSchools.length === 0 && (<Typography>Šajā apkaimē skolas vēl netika pievienotas.</Typography>)}
+      {filteredSchools && filteredSchools.length === 0 && (
+        <Typography>Šajā apkaimē skolas vēl netika pievienotas.</Typography>
+      )}
+      <Box sx={{ px: 2 }}>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <Typography>Attālums:</Typography>
+          <Typography sx={{ fontWeight: 500 }}>{radiusFilter} km</Typography>
+        </Box>
+        <Slider
+          aria-label="Custom marks"
+          value={radiusFilter}
+          valueLabelDisplay="auto"
+          onChange={handleRadiusFilter}
+          marks
+          step={1}
+          min={1}
+          max={5}
+        />
+      </Box>
       {filteredSchools && (
         <Box sx={{ mb: 2 }}>
           {filteredSchools.map((item) => (
